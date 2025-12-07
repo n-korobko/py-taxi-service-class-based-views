65d591167f1287b9dbd9260cc7cd2aa01878ba11
Nikita <hkorobko@gmail.com>
2025-12-07 20:43:05 +0200

    Taxi-service-class-based-views

--- taxi/models.py
+++ taxi/models.py
@@ -6,6 +6,9 @@ class Manufacturer(models.Model):
     name = models.CharField(max_length=255, unique=True)
     country = models.CharField(max_length=255)
 
+    def __str__(self):
+        return f"{self.name} ({self.country})"
+
 
 class Driver(AbstractUser):
     license_number = models.CharField(max_length=255, unique=True)
